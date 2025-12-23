9d4086cd8fd16b6054a67b0c662e7dec797bc037
Fauzan <141022549+MuhammadFauzanL@users.noreply.github.com>
2025-12-24 03:20:46 +0700

    Refactor logging and remove experiment setup
    Removed experiment setting and updated logging messages.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,9 +6,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import numpy as np
 
-# set experiment
-mlflow.set_experiment("Student_Performance_Experiment")
-
 df = pd.read_csv("student_performance_clean.csv")
 
 X = df.drop("FinalGrade", axis=1)
@@ -27,14 +24,14 @@ mae = mean_absolute_error(y_test, y_pred)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 r2 = r2_score(y_test, y_pred)
 
-# Loggiug
+# logging 
 mlflow.log_metric("MAE", mae)
 mlflow.log_metric("RMSE", rmse)
 mlflow.log_metric("R2", r2)
 
 mlflow.sklearn.log_model(model, "student_model")
 
-print("Training selesai")
+print("selesai")
 print("MAE :", mae)
 print("RMSE:", rmse)
 print("R2  :", r2)
